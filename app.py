284a0d4df5d14c22f965d92d08b3c519649e302d
pdfruth <pfruth@us.ibm.com>
2021-03-08 18:08:10 +0000

    update

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Electric")
-option_b = os.getenv('OPTION_B', "Petrol")
+option_a = os.getenv('OPTION_A', "Moderna")
+option_b = os.getenv('OPTION_B', "Pfizer")
 hostname = socket.gethostname()
 proc = platform.processor()
 
